0204e8b36d59a5525c7ece5ec81d6b0257838955
Victor Hernandez <victorferney14@hotmail.com>
2022-08-30 16:27:36 -0500

    Se añadio la conexion a la base de datos en properties y se creo el sevicio hello

--- src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
@@ -2,9 +2,18 @@ package com.UdeA.Ciclo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
+@RestController
 public class Ciclo3Application {
+
+	@GetMapping("/hello")
+	public String hello(){
+       return "Hola Ciclo 3";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
